5d5b4d28bdea8db3e2f53e0c03aae7d6680d2e44
SAKAI Yoshihito <y-sakai@esm.co.jp>
2018-08-06 13:29:32 +0900

    initializr

---
+++ src/main/kotlin/jp/co/esm/its/springjdbcui/SpringJdbcUiApplication.kt
@@ -0,0 +1,11 @@
+package jp.co.esm.its.springjdbcui
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJdbcUiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJdbcUiApplication>(*args)
+}
