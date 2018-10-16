6fd689da069ec7977d7f4f84d509d143952094c1
mk2 <haruka.asakura@esm.co.jp>
2018-10-16 14:54:21 +0900

    データ表示部分、クエリパラメータ設定部分のUI追加

--- src/javascript/src/components/QueryParametersEdit.vue
+++ src/javascript/src/components/QueryParametersEdit.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>EditQueryParameters</div>
+  <v-form>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md3>
+          <v-text-field label="ダミー"></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
@@ -8,3 +16,9 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+.container {
+  max-width: 100% !important;
+  padding: 0px !important;
+}
+</style>
--- src/javascript/src/components/QueryResult.vue
+++ src/javascript/src/components/QueryResult.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>QueryResult</div>
+  <v-container>
+    <v-data-table :items="data">
+      <template slot="items" slot-scope="props">
+        <td v-for="column in columns">
+          {{ props.item[column] }}
+        </td>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      columns: ['id', 'name'],
+      data: [
+        { id: 0, name: 'test0' },
+        { id: 1, name: 'test1' }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container {
+  max-width: 100% !important;
+  padding: 0px !important;
+}
 </style>
